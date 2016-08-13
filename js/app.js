4e40372a4b863f42708ac0fd81c4045ce833dcea
jeremypeskin <jeremy.p.peskin@gmail.com>
2016-08-13 15:26:51 -0400

    Undid local storage changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 var addButton = document.getElementById("add-button");
 var savedPropertiesTable = document.getElementById("saved-properties-table");
 var nameInput = document.getElementById("name-input");
-localStorage.setItem("savedName", nameInput.value);
-var savedName = localStorage.getItem("savedName")
 var priceInput = document.getElementById("price-input");
 var rentInput = document.getElementById("rent-input");
 var maintenanceInput = document.getElementById("maintenance-input");
